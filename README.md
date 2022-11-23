cd5d763121338137cfd48149b7cf22932e6a854d
Ivo Lopes <ivo.lopes@prit.app>
2022-11-23 09:14:24 -0300

    add informations in readme file

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ You can run your application in dev mode that enables live coding using:
 ```shell script
 ./mvnw compile quarkus:dev
 ```
+
+## Application information
+
+- The application was set up to run in the 8080 port.
+- The URL to access the swagger is http://localhost:8080/swagger-ui/
+- And if wanting to get the alticci sequence, it is possible through 
+this API endpoint (http://localhost:8080/alticci/{n}).
--- src/main/java/org/alticci/resources/AlticciResource.java
+++ src/main/java/org/alticci/resources/AlticciResource.java
@@ -1,13 +1,18 @@
 package org.alticci.resources;
 
-import org.alticci.exceptions.AlticciException;
-import org.alticci.services.AlticciService;
-
+import java.util.List;
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
+import org.alticci.exceptions.AlticciException;
+import org.alticci.services.AlticciService;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 @Path("/alticci")
 @Produces(MediaType.APPLICATION_JSON)
@@ -17,6 +22,12 @@ public class AlticciResource {
     @Inject
     AlticciService service;
 
+    @APIResponses({
+        @APIResponse(responseCode = "200", description = "Alticci sequence calculated successfully"),
+        @APIResponse(responseCode = "400", description = "If an invalide number was sent to " +
+            "calculate the alticci sequence"),
+        @APIResponse(responseCode = "500", description = "Internal error")
+    })
     @GET
     @Path("/{n}")
     public Response get(@PathParam("n") Integer number) throws AlticciException {
